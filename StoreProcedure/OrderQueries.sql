c3c4386bc9b81691a8aad9147361084c7685a8b2
unknown <s.nanthagopal1998@gmail.com>
2022-09-21 18:44:56 +0530

    [Nantha] - Order API Added

--- StoreProcedure/OrderQueries.sql
+++ StoreProcedure/OrderQueries.sql
@@ -1,5 +1,4 @@
 
-
 CREATE TABLE TrackOrder(
 OrderId INT IDENTITY(1,1) PRIMARY KEY,
 Quantity INT,
@@ -8,8 +7,9 @@ BookId INT FOREIGN KEY (BookId) REFERENCES Books(BookId),
 AddressId INT FOREIGN KEY (AddressId) REFERENCES Address(AddressId)
 )
 
-DROP TABLE TrackOrder
-ALTER TABLE TrackOrder ADD CartId INT FOREIGN KEY (CartId) REFERENCES Cart(CartId)
+ALTER TABLE TrackOrder DROP COLUMN CartId
+
+ALTER TABLE TrackOrder ADD CartId INT FOREIGN KEY (CartId) REFERENCES Cart(CartId) ON DELETE SET NULL
 ALTER TABLE TrackOrder ADD OrderCost INT NOT NULL
 ALTER TABLE TrackOrder ADD DateTime VARCHAR(MAX) NOT NULL
 
@@ -17,11 +17,9 @@ DELETE FROM TrackOrder WHERE OrderId = 2
 
 ALTER TABLE TrackOrder ALTER COLUMN OrderCost FLOAT NOT NULL 
 
-
-
-alter table TrackOrder add constraint fk_CARTID foreign key(CartId) references Cart(CartId) ON DELETE CASCADE ON UPDATE CASCADE;
-
 SELECT * FROM TrackOrder
+
+SELECT * FROM Cart
 
 
 
@@ -34,12 +32,17 @@ BEGIN TRY
 	DECLARE @BookId INT = (SELECT BookId FROM CART WHERE CartId = @CartId)
 	DECLARE @Cost FLOAT = (SELECT DiscountPrice FROM Books WHERE BookId = @BookId)
 	DECLARE @ActualQuantity INT = (SELECT BookQuantity FROM Books WHERE BookId = @BookId)
-	IF(@REQQuantity < @ActualQuantity)
+
+	IF(@ActualQuantity >= @REQQuantity  )
 	BEGIN
-	
 	DECLARE @OrderCost FLOAT = @Cost * @REQQuantity
-	INSERT INTO TrackOrder VALUES(@REQQuantity, @UserId, @BookId, @AddressId, @CartId, @OrderCost, @DateTime)
+	INSERT INTO TrackOrder VALUES (@REQQuantity, @UserId, @BookId, @AddressId, @OrderCost, @DateTime, @CartId)
+	UPDATE Books SET BookQuantity = @ActualQuantity - @REQQuantity WHERE BookId = @BookId
+	DELETE FROM Cart WHERE CartId = @CartId
 	END
 END TRY
 BEGIN CATCH
 END CATCH
+GO
+
+exec Place_Order 5,1,'Sep 21 2022 10:10AM'
